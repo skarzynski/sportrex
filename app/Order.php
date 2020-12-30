f27729b5ac0ea89af9989f36129154b7b97b9df8
Maciej Ryś <246648@pwr.edu.pl>
2020-12-30 20:22:37 +0100

    add functions in model Order and OrderStatus Factory Seeder and Controller

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable =['delivery_address','price','order_date','email'];
+
+    function delivery() {
+        return $this->belongsTo(Delivery::class);
+    }
+
+    function payment() {
+        return $this->belongsTo(Payment::class);
+    }
+
+    function orderStatus() {
+        return $this->belongsTo(OrderStatus::class);
+    }
+
+    function products() {
+        return $this->belongsToMany(Product::class)->withTimestamps();
+    }
+
+
 }
