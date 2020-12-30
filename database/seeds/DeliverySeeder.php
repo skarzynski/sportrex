15745b8fea65e6062cc93bffb0a36465b60b2e7f
Maciej Ryś <246648@pwr.edu.pl>
2020-12-30 19:31:51 +0100

    rename migrations, add models, seeds and controllers for dome objects

---
+++ database/seeds/DeliverySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DeliverySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
